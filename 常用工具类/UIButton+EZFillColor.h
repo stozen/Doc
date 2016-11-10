a8906a8ba5e8f521e8aad3c6ad6deecfa5170998
qyzhaojinxi <qyzhaojinxi@163.com>
2016-11-10 11:38:54 +0800

    创建常用工具类文件夹，方便管理

---
+++ 常用工具类/UIButton+EZFillColor.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+EZFillColor.h
+//  MyContacts
+//
+//  Created by 赵 进喜 on 15/1/28.
+//  Copyright (c) 2015年 everzones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (EZFillColor)
+-(void)setBackgroundColor:(UIColor *)backgroundColor forState:(UIControlState)state;
+@end
